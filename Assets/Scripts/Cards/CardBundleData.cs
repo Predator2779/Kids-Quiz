636406fdd497dfb15ae6e8bc7b67d04bd47e0c5f
Predator2779 <66646630+Predator2779@users.noreply.github.com>
2024-12-19 00:05:40 +0500

    added card data classes and created test SO

---
+++ Assets/Scripts/Cards/CardBundleData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Cards
+{
+    [CreateAssetMenu(fileName = "New CardBundleData", menuName = "Card Bundle Data", order = 10)]
+    public class CardBundleData : ScriptableObject
+    {
+        [SerializeField] private CardData[] _cardDatas;
+
+        public CardData[] CardDatas => _cardDatas;
+    }
+}
---
+++ Assets/Scripts/Cards/CardData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Cards
+{
+    [Serializable]
+    public class CardData
+    {
+        [SerializeField] private string _identifier;
+        [SerializeField] private Sprite _sprite;
+
+        public string Identifier => _identifier;
+        public Sprite Sprite => _sprite;
+    }
+}
